68858a66e74fbbb1ba4bd5a14d66267ee7fe9c4c
filipetaremba <filipetaremba0@gmail.com>
2026-04-20 07:15:05 +0200

    Update dowload

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -1,6 +1,16 @@
 import { Suspense } from "react";
 import DownloadPage from "@/components/DownloadPage";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function Page() {
-  return <Suspense><DownloadPage /></Suspense>;
+  return (
+    <>
+      <Navbar />
+      <Suspense>
+        <DownloadPage />
+      </Suspense>
+      <Footer />
+    </>
+  );
 }
