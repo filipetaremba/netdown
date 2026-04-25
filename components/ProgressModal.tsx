780f521c99791dc18ab45aefc2a1a0e15b3906f2
filipetaremba <filipetaremba0@gmail.com>
2026-04-25 08:53:50 +0200

    Update Project

--- components/ProgressModal.tsx
+++ components/ProgressModal.tsx
@@ -43,7 +43,7 @@ export default function ProgressModal({ onComplete }: ProgressModalProps) {
           requestAnimationFrame(animate);
         } else {
           stageIndex++;
-          setTimeout(runStage, 50);
+          setTimeout(runStage, 100);
         }
       };
 
